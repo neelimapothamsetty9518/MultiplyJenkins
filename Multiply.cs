438180edf4759dbafa22d3a7562979b31685bc3e
neelimapothamsetty9518 <neelimapothamsetty1@gmail.com>
2024-02-15 16:01:03 +0530

    added all files

---
+++ Multiply.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplyjenkins
+{
+    public class Multiply
+    {
+        //int size = 5;
+        public  bool Mul(int size)
+        {
+            for(int i=1;i<=size;i++)
+            {
+                for(int j=1;j<=size;j++)
+                {
+                    int result = i * j;
+                    Console.WriteLine(result);
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using multiplyjenkins;
+
+Console.WriteLine("this is a mul table");
+Multiply mult = new Multiply();
+Console.Write(mult.Mul(5));
+
